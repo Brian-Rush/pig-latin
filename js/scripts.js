c70eda820e94be29b2db24e34cd481cbc118065a
Brian Rush and Maria Del Castillo <brian+maria@>
2017-05-03 12:10:30 -0700

    add logic to remove first letter of input and append it with 'ay'.

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,34 @@
+//Back-End logic
+
+//append way
+var addWay= function() {
+  $("#result").text(input + "way");
+};
+
+var sliceFirst = function(input) {
+  var x = input.slice(1);
+  return x;
+};
+
+
+//Front-End Logic
 $(document).ready(function() {
   $("form#english").submit(function(event) {
     event.preventDefault();
 
+    //Get and store input
     var input = $("#eng").val();
-    var pigLatin = function(x) {
-      $("#result").text(input + "way");
-    }
-    pigLatin();
+
+
+    addWay;
+
+    var firstLetter = input.charAt(0);
+    var restOfWord = sliceFirst(input);
+
+    var consonants = ["b", "c", "d", "f", "g", "h", "j", "k", "l", "m", "n", "p", "q", "r", "s", "t", "v", "w", "x", "y", "z"]
+      if (consonants.includes(firstLetter)) {
+        $("#result").text(restOfWord + firstLetter + "ay");
+      }
+  // alert(firstLetter);
   });
 });
