3157adf29323543e0010c359a99d2c0f13a635af
Brian Rush and Maria Del Castillo <brian+maria@>
2017-05-03 16:52:24 -0700

    add logic to handle multi-letter words beginning with a single consonant, and differentiate between single-letter words, words that begin with a vowel, and words that begin with a consonant

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,57 @@
 //Back-End logic
 
-//append way
-var addWay= function() {
-  $("#result").text(input + "way");
-};
+//random global variables
+var consonants = ["b", "c", "d", "f", "g", "h", "j", "k", "l", "m", "n", "p", "q", "r", "s", "t", "v", "w", "x", "y", "z"]
 
-var sliceFirst = function(input) {
-  var x = input.slice(1);
+//cuts off first letter and returns rest of the input
+var sliceFirst = function(word) {
+  var x = word.slice(1);
   return x;
 };
 
+//determines if first letter is consonant and returns pig latin
+var foo = function(word){ // "rum"
+  var firstLetter = word.charAt(0); // "r"
+
+  if (consonants.includes(firstLetter)) {
+      // $("#result").text(result);
+      return sliceFirst(word) + firstLetter + "ay";
+  }
+}
+
+var foo2 = function(x) {
+	if (x.length === 1) {
+  	return x + "way";
+  } else {
+  return foo(x);
+  }
+}
+
+
+//splits input into individual characters
+// var letters = input.split("");
+var initialConsonants = [];
+
+// for (index = 0; index === true; index += 1) { // use while
+//   if (consonants.includes(letter[index])) {
+//     return true;
+//     (initialConsonants).push(letter)
+//
+//     });
+
+var multiCons = function() {
+  while (consonants.includes(y)) {
+    (initialConsonants).push(y)
+  };
+alert(initialConsonants);
+};
+// };
+
+
 
 //Front-End Logic
+
+
 $(document).ready(function() {
   $("form#english").submit(function(event) {
     event.preventDefault();
@@ -19,16 +59,10 @@ $(document).ready(function() {
     //Get and store input
     var input = $("#eng").val();
 
+    var result = foo2(input); // return a result
 
-    addWay;
+    // display result with jquery
+    $("#result").text(result);
 
-    var firstLetter = input.charAt(0);
-    var restOfWord = sliceFirst(input);
-
-    var consonants = ["b", "c", "d", "f", "g", "h", "j", "k", "l", "m", "n", "p", "q", "r", "s", "t", "v", "w", "x", "y", "z"]
-      if (consonants.includes(firstLetter)) {
-        $("#result").text(restOfWord + firstLetter + "ay");
-      }
-  // alert(firstLetter);
   });
 });
