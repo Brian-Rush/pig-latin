e521f26c3ac7f65c8c641d29376ec6de603739e9
Brian Rush and Maria Del Castillo <brian+maria@>
2017-05-03 18:03:16 -0700

    add uppercase consonants to the consonants array

--- js/scripts.js
+++ js/scripts.js
@@ -1,31 +1,44 @@
 //Back-End logic
 
-//random global variables
-var consonants = ["b", "c", "d", "f", "g", "h", "j", "k", "l", "m", "n", "p", "q", "r", "s", "t", "v", "w", "x", "y", "z"]
+//GLOBAL VARIABLES
+
+//consonant array
+var consonants = ["b", "c", "d", "f", "g", "h", "j", "k", "l", "m", "n", "p", "q", "r", "s", "t", "v", "w", "x", "y", "z", "B", "C", 'D', 'F', 'G', 'H', 'J', 'K', 'L', 'M', 'N', 'P', 'Q', 'R', 'S', 'T', 'V', 'W', 'X', 'Z'];
+
+//isolates first letter of word
+var firstLetter = function (word) {
+  word.charAt(0);
+};
 
 //cuts off first letter and returns rest of the input
-var sliceFirst = function(word) {
+var restOfWord = function (word) {
   var x = word.slice(1);
   return x;
 };
 
 //determines if first letter is consonant and returns pig latin
-var foo = function(word){ // "rum"
-  var firstLetter = word.charAt(0); // "r"
-
+var foo = function (word) {
+  var firstLetter = word.charAt(0);
   if (consonants.includes(firstLetter)) {
-      // $("#result").text(result);
-      return sliceFirst(word) + firstLetter + "ay";
-  }
-}
+      return restOfWord(word) + firstLetter + "ay";
+  };
+};
 
-var foo2 = function(x) {
+//determines if first letter is not a consonant and returns apt pig latin
+// var initVowel = function(word) {
+//
+//   if (!consonants.includes(firstLetter)) {
+//     return word + "way";
+//   };
+// };
+
+var foo2 = function (x) {
 	if (x.length === 1) {
   	return x + "way";
   } else {
   return foo(x);
   }
-}
+};
 
 
 //splits input into individual characters
@@ -39,12 +52,12 @@ var initialConsonants = [];
 //
 //     });
 
-var multiCons = function() {
-  while (consonants.includes(y)) {
-    (initialConsonants).push(y)
-  };
-alert(initialConsonants);
-};
+// var multiCons = function() {
+//   while (consonants.includes(y)) {
+//     (initialConsonants).push(y)
+//   };
+// alert(initialConsonants);
+// };
 // };
 
 
@@ -52,14 +65,15 @@ alert(initialConsonants);
 //Front-End Logic
 
 
-$(document).ready(function() {
-  $("form#english").submit(function(event) {
+$(document).ready(function () {
+  $("form#english").submit(function (event) {
     event.preventDefault();
 
     //Get and store input
     var input = $("#eng").val();
 
-    var result = foo2(input); // return a result
+    // return a result
+    var result = foo2(input);
 
     // display result with jquery
     $("#result").text(result);
